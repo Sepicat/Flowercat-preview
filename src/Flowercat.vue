58795676ae25888e11b0725c706c9d849d89b7c5
Harry Twan <gua@desgard.com>
2018-04-26 15:37:30 +0800

    Initial the vue project for flowercat-preview
    Signed-off-by: Harry Twan <gua@desgard.com>

---
+++ src/Flowercat.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Flowercat
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Demo</p>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data () {
+    return {
+      msg: 'This is my demo!!!'
+    }
+  }
+}
+</script>
+
