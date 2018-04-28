54041683f5d7e02408ac688f34cca3a3d809a5f5
Harry Twan <gua@desgard.com>
2018-04-28 09:20:49 +0800

    creat a todolist sample for vue learn
    Signed-off-by: Harry Twan <gua@desgard.com>

--- src/Flowercat.vue
+++ src/Flowercat.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    Flowercat
+  <div id="app">
+    <h1 v-html="msg"></h1>
+    <input type="text" v-model="itemNew" v-on:keyup.enter="addNew" />
+
+    <ul>
+    </ul>
   </div>
 </template>
 
 <script>
+
+import Storage from './localStorage'
+
 export default {
+  data () {
+    return {
+      msg: 'To-do list',      
+      items: Storage.fetch(),
+      
+    }
+  }
 }
 </script>
-
-<style>
-</style>
